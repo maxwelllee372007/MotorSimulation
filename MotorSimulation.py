5866d0e051d25cf25abe5b2d5448c211d045f828
maxwell.lee.372007 <maxwell.lee.372007@gmail.com>
2025-03-04 14:16:21 -0500

    add gearbox options to simulation

--- MotorSimulation.py
+++ MotorSimulation.py
@@ -1,8 +1,8 @@
 import csv
-# gearRatio = 50.0 # geared 50.0x slower
+gearRatio = 5.0 # geared 50.0x slower
 inertia = 0.1 # 10.0 kgm^2 mechanism inertia without gearbox
 startVelocity = 0.0 # mechanism starts at 0.0 RPM
-distanceToTravel = 35.0 # mechanism must travel 5.0 rotations (without gearbox factored in)
+distanceToTravel = 305.0 # mechanism must travel 5.0 rotations (without gearbox factored in)
 timeStep = 0.1 # simulate numerical approximation every 0.1 seconds
 
 # function to access torque values from csv file
@@ -17,11 +17,11 @@ currentPosition = 0.0
 currentVelocity = startVelocity
 currentTime = 0.0
 
-# main loop
+# main simulation loop
 while (currentPosition < distanceToTravel) :
-    acceleration = getTorque(currentVelocity * 60) / inertia
+    acceleration = getTorque(currentVelocity * 60 * gearRatio) / inertia / gearRatio
     currentVelocity += acceleration * timeStep
     currentPosition += currentVelocity * timeStep
     currentTime += timeStep
     print(str(round(currentTime,1)) + "s - " + "pos: " + str(round(currentPosition,2)) + " rot - vel: " + str(round(currentVelocity,2)) + " rot/s - accel:" + str(round(acceleration,2)) + " rot/s/s")
-print ("total time for a mechanism with inertia of " + str(inertia) + " kgm^2 to travel " + str(distanceToTravel) + " rotations: " + str(round(currentTime,2)) + " seconds")
+print ("when mounted on a " + str((gearRatio)) + ":1 gearbox, the total time for a mechanism with inertia of " + str(inertia) + " kgm^2 to travel " + str(distanceToTravel) + " rotations is " + str(round(currentTime,2)) + " seconds")
